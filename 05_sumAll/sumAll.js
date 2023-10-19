1f75127fd3f915a0fc4ec155899aa288b8b67ed3
Anna Maria <123685928+annalewke@users.noreply.github.com>
2023-10-19 17:34:29 +0200

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (start < 0 | end < 0) {
+        sum = "ERROR";
+    } else if (typeof (start) === "number" && typeof (end) === "number") {
+        if (start > end) {
+            let i = end;
+            while (i <= start) {
+                sum += i;
+                i++;
+            }
+        } else {
+            let i = start;
+            while (i <= end) {
+                sum += i;
+                i++;
+            }
+        }
+    } else {
+        sum = "ERROR";
+    }
+    return sum;
 };
 
 // Do not edit below this line
