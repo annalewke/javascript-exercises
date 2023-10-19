ffbc2daad908c8befa4592ad4d30e2edf727143d
Anna Maria <123685928+annalewke@users.noreply.github.com>
2023-10-19 17:32:58 +0200

    Complete exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+   let stringList = string.split(" ");
+   const mixList = [];
+   for (const word of stringList) {
+    let mix = word.split("").reverse().join("");
+    mixList.push(mix);
+   }
+   return mixList.reverse().join(" ");
 };
 
 // Do not edit below this line
