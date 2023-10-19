08bdc3fffcce18cf47aedd86cd7d81d7ba9de805
Anna Maria <123685928+annalewke@users.noreply.github.com>
2023-10-19 17:33:45 +0200

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(add, ...removeList) {
+    const arrayList = add;
+    for (const removeItem of removeList) {
+        let i = 0;
+        while (i < arrayList.length) {
+            if (removeItem === arrayList[i]) {
+                arrayList.splice(i,1);
+            }
+            i++;
+        }
+    }
+    return arrayList;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
